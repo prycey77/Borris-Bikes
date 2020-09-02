a3b5e7962de863ac1797e1cb9b40beceb72d7b2f
Louis Kirkham <louiskirkham@gmail.com>
2020-09-02 20:14:30 +0100

    added test to check working? method on DockingStation

--- spec/borris_bikes_spec.rb
+++ spec/borris_bikes_spec.rb
@@ -1,12 +1,20 @@
 require "borris_bikes"
 
 
-RSpec.describe DockingStation do
+describe DockingStation do
+  it {is_expected.to respond_to(:release_bike)} 
   
-  it {is_expected.to respond_to(:release_bike)}
+  describe (DockingStation.new).release_bike do
+    it {is_expected.to respond_to(:working?)}
+  end
 
 end
 
-RSpec.describe DockingStation.release_bike do
-  it {is_expected.to respond_to(:working?)}
-end
+# describe DockingStation do
+
+#   describe release_bike do
+#     expect(subject.release_bike).to respond_to(subject.working?)
+#   end
+
+# end
+
