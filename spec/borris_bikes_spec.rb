dabde80039a733fa03074de223a31cb829a698ed
Charlotte Brandhorst-Satzkorn <catzkorn@gmail.com>
2020-09-02 16:22:43 +0100

    Exercise 8 updated

--- spec/borris_bikes_spec.rb
+++ spec/borris_bikes_spec.rb
@@ -2,6 +2,6 @@ require "borris_bikes"
 
 describe DockingStation do
   it "docking station responds to release bike" do
-    expect DockingStation.respond_to?(:release_bike)
+    expect(DockingStation).respond_to?(release_bike)
   end
 end
